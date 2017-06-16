b28b2df5b804e29364a42bcbe24b9135082ad127
Marcos Antonio Blanco <mablanco@devsdna.com>
2017-06-16 17:47:35 +0200

    Added UITests project.

---
+++ Krillin/Krillin.App/Krillin.App/Krillin.App.UITests/AppInitializer.cs
@@ -0,0 +1,34 @@
+﻿namespace Krillin.App.UITests
+{
+    using System;
+    using Xamarin.UITest;
+
+    
+    public class AppInitializer
+    {
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="platform">The platform.</param>
+        /// <returns></returns>
+        /// <exception cref="System.PlatformNotSupportedException"></exception>
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .StartApp();
+            }
+            else if (platform == Platform.iOS)
+            {
+                return ConfigureApp
+                    .iOS
+                    .StartApp();
+            }
+
+            throw new PlatformNotSupportedException($"{platform} is not supported.");
+        }
+    }
+}
+
---
+++ Krillin/Krillin.App/Krillin.App/Krillin.App.UITests/Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Krillin.App.UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            this.app = AppInitializer.StartApp(this.platform);
+            this.app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void WhenInitApp_ListViewIsFilled()
+        {
+            AppResult[] result = this.app.Query(x => x.Marked("ListPost").Child());
+            Assert.IsTrue(result.Length > 0);
+        }
+    }
+}
+
